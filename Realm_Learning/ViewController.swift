20737f0b710cd4a5f0b47fce01eb11b07d261b7f
Shahrukh Shikalgar <shahrukh.shikalgar@neosofttech.com>
2020-03-02 12:50:32 +0530

    Initial Commit

---
+++ Realm_Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm_Learning
+//
+//  Created by Neosoft on 02/03/20.
+//  Copyright © 2020 Neosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
